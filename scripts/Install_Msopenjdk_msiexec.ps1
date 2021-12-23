7ed470cbf45d305b059068480b4efd733bb30eb6
Your Name <you@example.com>
2021-12-23 14:20:03 +0300

    PSDebug

--- scripts/Install_Msopenjdk_msiexec.ps1
+++ scripts/Install_Msopenjdk_msiexec.ps1
@@ -1,6 +1,6 @@
 #--------------------------------------------------------------------------------------------------------
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-psdebug?view=powershell-7.1
-Set-PSDebug -Trace 2 #turns script debugging features on and off, sets the trace level
+# Set-PSDebug -Trace 2 #turns script debugging features on and off, sets the trace level
 
 $VerbosePreference = "continue"
 Write-Output $VerbosePreference
