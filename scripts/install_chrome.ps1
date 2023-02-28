af1a025d2f3a65179624883feeeb0e8b4add5a8e
Your Name <you@example.com>
2023-02-28 16:15:24 +0300

    chrome

---
+++ scripts/install_chrome.ps1
@@ -0,0 +1,26 @@
+﻿
+$chrome_url = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+$chrome_path = "C:\tmp\chrome_installer.exe"
+
+
+if(Test-Path $chrome_path) {
+  Write-Host "Google Chrome is already downloaded and installed."
+} else {
+  Write-Host "Downloading Google Chrome..."
+  Invoke-WebRequest -Uri $chrome_url -OutFile $chrome_path
+  
+  if(Test-Path $chrome_path) {
+    Write-Host "Google Chrome has been downloaded successfully."
+  } else {
+    Write-Warning "Failed to download Google Chrome. Please check the URL and try again."
+    Exit
+  }
+}
+
+Write-Host "Installing Google Chrome..."
+Start-Process -FilePath $chrome_path -Args '/silent /install' -Verb RunAs -Wait; 
+Write-Host "Google Chrome has been installed successfully."
+
+Remove-Item -Path $Path\$Installer
+
+
---
+++ scripts/uninstall_chrome.ps1
@@ -0,0 +1,12 @@
+﻿# Check if Google Chrome is installed
+$chrome = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "Google Chrome" }
+
+if ($chrome -eq $null) {
+    Write-Host "Google Chrome is not installed."
+} else {
+    # Uninstall Google Chrome
+    Write-Host "Uninstalling Google Chrome..."
+    $uninstall = $chrome.UninstallString -replace "/install", "/uninstall"
+    Start-Process cmd.exe -ArgumentList "/c", "$uninstall /quiet /norestart" -Wait
+    Write-Host "Google Chrome has been uninstalled."
+}
