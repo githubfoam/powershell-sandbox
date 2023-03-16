4d683e4b2d60145eb1f1fb1654eb9a97c5a9c308
Your Name <you@example.com>
2023-03-16 13:28:12 +0300

    upgrade

---
+++ scripts/upgrade_powershell.ps1
@@ -0,0 +1,22 @@
+﻿# Get the current version of PowerShell
+$currentVersion = $PSVersionTable.PSVersion.ToString()
+Write-Host "Current version of PowerShell: $currentVersion"
+
+# Get the latest stable version of PowerShell
+$latestRelease = Invoke-RestMethod "https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
+$latestVersion = $latestRelease.tag_name.Replace("v", "")
+Write-Host "Latest version of PowerShell: $latestVersion"
+
+# Check if a new stable release is available
+if ([version]$latestVersion -gt [version]$currentVersion) {
+    # Download the PowerShell installer for the latest release
+    $downloadUrl = $latestRelease.assets | Where-Object { $_.name -like "*win-x64.msi" } | Select-Object -ExpandProperty browser_download_url
+    $installerPath = "$env:TEMP\PowerShell-$latestVersion-win-x64.msi"
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
+
+    # Install the latest stable version of PowerShell
+    Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart" -Wait
+    Write-Host "PowerShell has been successfully upgraded to version $latestVersion."
+} else {
+    Write-Host "You are already using the latest version of PowerShell."
+}
